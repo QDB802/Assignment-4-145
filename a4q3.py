acdfe9b4a7d19f600673eed189d1cfcb6d37e8fb
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-07-09 15:02:25 -0600

    Second Commit for a4q3
     * Worked on a4q3.py.
     * Worked on a4q3_testing.py, test driver has revealed an error with the a4q3 function, and that several "|" will appear after multi-node chains

---
+++ a4q3.py
@@ -0,0 +1,33 @@
+# Name: Ken Duff
+# NSID: qdb802
+# Student#: 11318955
+# CMPT 145
+# Instructor: Lauresa Stilling
+
+def to_string(node_chain):
+    """
+    Purpose:
+        Converts the node chain into a string representation.
+    Parameters:
+        node_chain: The chain of  nodes that will be converted, either into a string representation or simply will be
+        returned empty.
+    Returns:
+        The string representation of the node, or the string 'EMPTY'.
+    """
+
+    # Checks if the node_chain is empty
+    if node_chain is None:
+        return 'EMPTY'
+    else:
+        # Copies the value from the current node, then formats it, and finally cycles to next node.
+        node_value = node_chain.get_data()
+        result = '[ {} |'.format(str(node_value))
+        next_node = node_chain.get_next()
+    if next_node is None:
+        # If the node chain has reached its end, it caps it off with this.
+        result += ' / ]'
+    else:
+        result += ' *-]-->[ {} |'.format(str(to_string(next_node)))
+
+    return result
+
---
+++ a4q3_testing.py
@@ -0,0 +1,46 @@
+# Name: Ken Duff
+# NSID: qdb802
+# Student#: 11318955
+# CMPT 145
+# Instructor: Lauresa Stilling
+
+from a4q3 import to_string
+import node as n
+
+def test_driver():
+    fails = 0
+    tests = 0
+
+    # Testing an empty node chain
+    test_node = None
+    expected = 'EMPTY'
+    result = to_string(test_node)
+    if result != expected:
+        fails += 1
+        print("Testing to_string() with", test_node, "   Expected:", expected, " Got: ", result)
+    tests += 1
+
+    # Testing a one node chain
+    test_node = n.node(5)
+    expected = '[ 5 | / ]'
+    result = to_string(test_node)
+    if result != expected:
+        fails += 1
+        print("Testing to_string() with", test_node, "   Expected:", expected, " Got: ", result)
+    tests += 1
+
+    # Testing a multi-node chain, all the same type (int).
+    test_node = n.node(1, n.node(2, n.node(3)))
+    expected = '[ 1 | *-]-->[ 2 | *-]-->[3 | / ]'
+    result = to_string(test_node)
+    if result != expected:
+        fails += 1
+        print("Testing to_string() with", test_node, "   Expected:", expected, " Got: ", result)
+    tests += 1
+
+    print(f"Tests Ran: {tests}" "\n"
+          f"Tests Succeeded: {tests-fails}/{tests}" "\n"
+          f"Tests Failed: {fails}")
+
+
+test_driver()
