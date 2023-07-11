02fab62cebed81874b17be1924937643a82739d1
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-07-11 16:22:48 -0600

    Fourth Commit for a4q4
     * Created the move_validity function. It checks the validity of a attempted move in the maze, and either returns False if invalid, or True if valid.

--- a4q4.py
+++ a4q4.py
@@ -3,13 +3,16 @@
 # Student#: 11318955
 # CMPT 145
 # Instructor: Lauresa Stilling
+
 import os as os
+
+
 def maze_conversion(input_maze):
     """
         Purpose:
             Converts the initial maze file into a list of lists maze.
         Parameters:
-            input_file: The initial maze file that will be converted
+            input_maze: The initial maze file that will be converted
         Return:
             A list of lists which contain the maze layout
     """
@@ -19,6 +22,29 @@ def maze_conversion(input_maze):
     maze = [[value for value in line.rstrip() if value != ' '] for line in lines]
     f.close()
     return maze
+
+
+def move_validity(lst, current_row, current_col):
+    """
+        Purpose:
+            Checks the validity of the current move in order to determine if the move is allowed.
+        Parameters:
+            lst: The maze list of lists.
+            current_row: An integer of the current row position.
+            current_col: An integer of the current column position.
+        Return:
+            A Boolean determining the move validity, True for a valid move, False for an invalid move.
+    """
+    row_numbers = len(lst[1])
+    col_numbers = len(lst[0])
+
+    # If the move is attempting to backtrack or enter a wall, it will fail.
+    if lst[current_row][current_col] != 0:
+        return False
+    if (current_row < 0 or current_row >= row_numbers) or (current_col < 0 or current_col >= col_numbers):
+        return False
+    else:
+        return True
 
 
 def SolveMaze(maze, current, goal):
@@ -40,6 +66,10 @@ def SolveMaze(maze, current, goal):
     file_name, file_ext = os.path.splitext(maze)
     output_file_name = file_name + "-completed" + file_ext
 
+    # Checks the move validity to ensure move is not going out of bounds or repeating.
+    if not move_validity(maze_list, current_row, current_col):
+        return False
+
     # If the maze's start and end are at the exact same position, it will convert the location to a 'P' then
     # output the finished maze.
     if current_row == target_row and current_col == target_col:
@@ -49,9 +79,4 @@ def SolveMaze(maze, current, goal):
             g.write(' '.join(row) + '\n')
 
 
-
-
-
-
-
-SolveMaze("Maze1.txt", (1,1), (1,1))
+SolveMaze("Maze1.txt", (1, 1), (1, 1))
