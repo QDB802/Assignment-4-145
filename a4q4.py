9f2b8307954ecf238a270bb52edd2bd07c086b93
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-07-12 11:14:23 -0600

    Sixth Commit for a4q4
     * Currently facing index issues, attempt to diagnose the cause.
     * Making commit here incase if backtracking is necessary.

--- a4q4.py
+++ a4q4.py
@@ -35,7 +35,7 @@ def move_validity(lst, current_row, current_col):
         Return:
             A Boolean determining the move validity, True for a valid move, False for an invalid move.
     """
-    row_numbers = len(lst[1])
+    row_numbers = len(lst)
     col_numbers = len(lst[0])
 
     # If the move is attempting to backtrack or enter a wall, it will fail.
@@ -47,48 +47,51 @@ def move_validity(lst, current_row, current_col):
         return True
 
 
-def SolveMaze(maze, current, goal):
+def solvemaze(maze_list, current, goal):
     """
-    Purpose:
-        Converts the initial maze file into a list of lists maze.
-    Parameters:
-        maze: The initial maze file to be converted into the list of lists and explored.
-        current: A tuple of the starting position or current position for the maze.
-        goal: A tuple of the ending coordinates.
-    Return:
+       Purpose:
+           Converts the initial maze file into a list of lists maze.
+       Parameters:
+           maze_list: The initial maze file to be converted into the list of lists and explored.
+           current: A tuple of the starting position or current position for the maze.
+           goal: A tuple of the ending coordinates.
+       Return:
     """
-    # Converts the maze into the list of lists, and converts the tuples into single coordinates.
-    maze_list = maze_conversion(maze)
     current_row, current_col = current
     target_row, target_col = goal
-
-    # Using the os tools in order to add the completed into the file name to organize the files.
-    file_name, file_ext = os.path.splitext(maze)
-    output_file_name = file_name + "-completed" + file_ext
 
     # If the maze's current position and end are at the exact same position, it will convert the location to a 'P', then
     # output the finished maze.
     if current_row == target_row and current_col == target_col:
-        g = open(output_file_name, 'w')
         maze_list[current_row][current_col] = 'P'
-        for row in maze_list:
-            g.write(' '.join(row) + '\n')
-        g.close()
+        return True
 
     # Checks the move validity to ensure move is not going out of bounds or repeating.
     if not move_validity(maze_list, current_row, current_col):
         return False
-
+    # Sets the current space to a P, to ensure no backtracking and show where path is.
     maze_list[current_row][current_col] = "P"
 
-    for ra, ca in [(-1, 0), (0, -1), (1, 0), (0,1)]:
+    # List of Tuples containing all possible moves in the 4 directions.
+    for ra, ca in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
         new_row = current_row + ra
         new_col = current_col + ca
-        SolveMaze(maze_list, (new_row, new_col), goal)
+        solvemaze(maze_list, (new_row, new_col), goal)
 
-    maze_list[current_row][current_col] = '0'
-    return False
+def main():
+    maze_file = "Maze1.txt"
+    maze = maze_conversion(maze_file)
+    start_position = (0, 3)
+    goal_position = (4, 5)
+
+    if solvemaze(maze, start_position, goal_position):
+        # Using the os tools to modify the output file name for completed maze.
+        file_name, file_ext = os.path.splitext(maze_file)
+        output_file_name = file_name + "-completed" + file_ext
+
+        f = open(output_file_name, 'w')
+        for row in maze:
+            f.write(' '.join(row) + '\n')
 
 
-
-SolveMaze("Maze1.txt", (0, 3), (4, 5))
+main()
